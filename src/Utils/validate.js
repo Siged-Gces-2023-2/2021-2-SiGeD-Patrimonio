a4aa52784e5df5bfad80b2886e93a832e0c6913c
Bruno Oliveira <brunooliveira.unb@gmail.com>
2023-11-15 21:00:15 -0300

    Corrigindo erro ao adicionar caracteres especiais e acentuação na criação de um novo patrimonio
    Co-authored-by: Gustavo Barbosa <gustavobarbosa7162@gmail.com>

--- src/Utils/validate.js
+++ src/Utils/validate.js
@@ -1,20 +1,21 @@
 const validateName = (name) => {
-  const regex = /^[a-zA-Z ]{2,50}$/;
-  return (regex.test(name) && name !== undefined);
+  const regex = /^[\s\S]{2,50}$/;
+  return regex.test(name) && name !== undefined;
 };
 
 const validateDescription = (description) => {
-  const regex = /^[a-zA-Z ]{2,50}$/;
-  return (regex.test(description) && description !== undefined);
+  const regex = /^[\s\S]{2,200}$/;
+  return regex.test(description) && description !== undefined;
 };
 
 const validate = (name, description) => {
   const err = [];
 
   if (!validateName(name)) {
-    err.push('invalid name');
-  } if (!validateDescription(description)) {
-    err.push('invalid description');
+    err.push("invalid name");
+  }
+  if (!validateDescription(description)) {
+    err.push("invalid description");
   }
   return err;
 };
